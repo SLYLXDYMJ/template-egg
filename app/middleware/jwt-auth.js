118bff1b71d6471e972b273a62f74be1f6f10d12
LAPTOP-5PT00PJJ\jason <596908546@qq.com>
2020-09-17 16:40:36 +0800

    no message

--- app/middleware/jwt-auth.js
+++ app/middleware/jwt-auth.js
@@ -23,6 +23,8 @@ jwtAuth.init = function (app) {
   )
   
   app.passport.verify(async (ctx, jwtPayload) => {
+    // 这里应该根据 jwtPayload 的用户数据查询数据库
+    // 并返回用户数据 或 null
     return { id: 10086, name: '静态数据' }
   })
 }
