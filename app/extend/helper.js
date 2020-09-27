93dd1926f579c5008c86c03fbab6befa05b94b6b
LAPTOP-5PT00PJJ\jason <596908546@qq.com>
2020-09-27 12:04:31 +0800

    优化 formatOperatorQuery 函数

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -17,7 +17,8 @@ exports.success = function (data, msg = '√') {
  *  格式化查询对象
  **/
 exports.formatOperatorQuery = function (query) {
-  let itemResult = {}
+  let result = {}
+  let innerResult = {}
   let supportQuery = _.pick(query, [
     'eq', 'ne',
     'in', 'notIn',
@@ -25,65 +26,113 @@ exports.formatOperatorQuery = function (query) {
     'like', 'notLike'
   ])
   
-  _.each(supportQuery, (val, operator) => {
-    let tmpResult = {}
-    
-    // [['id:1'], ['id:2'], ['type:web']]
-    _.each(val.split(','), itemArr => {
-      // [['id', 1], ['id', 2], ['type', 'web']]
-      let [ key, value ] = itemArr.split(':')
-      
-      // value 若为 number 类型则转化
-      value = Number(value) || value
-      
-      // in 和 like 操作符值特殊处理
-      switch (operator) {
-        case 'in':
-        case 'notIn' : {
-          value = [ value ]
-          break
-        }
-        case 'like':
-        case 'notLike' : {
-          value = `%${ value }%`
-          break
-        }
-      }
-      
-      // 默认为对象
-      tmpResult[ key ] = tmpResult[ key ] || {
-        operator,
-        value: null
-      }
-      
-      // 整合值
-      tmpResult[ key ].value = tmpResult[ key ].value ?
-        [
-          ..._.flatten([ tmpResult[ key ].value ]),
-          ..._.flatten([ value ])
-        ] :
-        value
-    })
-    
-    // { id: { operator: 'in', value: [ 1, 2 ] }, type: { operator: 'in', value: 'web' } }
-    _.each(tmpResult, (data, key) => {
-      let { operator, value } = data
-      
-      itemResult[ key ] = itemResult[ key ] || {
-        [ Op.and ]: []
-      }
-      
-      itemResult[ key ][ Op.and ].push({
-        [ Op[ operator ] ]: value
+  /**
+   *  构建 innerResult
+   **/
+  _.each(supportQuery, (qsStr, opStr) => {
+    _.chain(qsStr)
+      .split(',')
+      .tap(result => {
+        /**
+         *  before:
+         *    id:1,id:2,type:web
+         *    ------------
+         *    name:jason
+         *  after:
+         *    [ 'id:1', 'id:2', 'type:web' ]
+         *    ------------
+         *    [ 'name:jason' ]
+         **/
+        // console.log(result)
       })
-    })
+      .map(qsItemStr => {
+        return _.split(qsItemStr, ':')
+      })
+      .tap(result => {
+        /**
+         *  before:
+         *    [ 'id:1', 'id:2', 'type:web' ]
+         *    ------------
+         *    [ 'name:jason' ]
+         *  after:
+         *    [ [ id, 1 ], [ id, 2 ], [ type, web ] ]
+         *    ------------
+         *    [ [ name, jason ] ]
+         **/
+        // console.log(result)
+      })
+      .each(qsItem => {
+        /**
+         *  [ id, 1 ]
+         **/
+        let [ key, value ] = qsItem
+        
+        /**
+         *  value 若可以转为 number 则转换
+         *  防止数据库查询时类型带来的问题
+         **/
+        value = Number(value) || value
+        
+        /**
+         *  in，like 操作符
+         *  value 转换
+         **/
+        switch (opStr) {
+          case 'in':
+          case 'notIn' : {
+            value = [ value ]
+            break
+          }
+          case 'like':
+          case 'notLike' : {
+            value = `%${ value }%`
+            break
+          }
+        }
+        
+        /**
+         *  初始化对应 key 值的查询对象
+         *  ex:
+         *    innerResult = { id: {} }
+         **/
+        innerResult[ key ] = innerResult[ key ] || {}
+        
+        /**
+         *  整合操作符查询条件
+         *    ex:
+         *      innerResult.id.in = [ 1, 2 ]
+         *      innerResult.type.eq = 'php'
+         **/
+        innerResult[ key ][ opStr ] = innerResult[ key ][ opStr ] ?
+          _.concat([], innerResult[ key ][ opStr ], value) :
+          value
+      })
+      .tap(() => {
+        /**
+         *  innerResult = {
+         *    id: { in: [ 1, 2 ] },
+         *    type: { eq: 'web', in: [ 'web' ] }
+         *  }
+         **/
+      })
+      .value()
   })
   
-  return {
-    [ Op.and ]: _.map(itemResult, (item, key) => {
-      return {
-        [ key ]: item
-      }
+  /**
+   *  根据 innerResult 构建 result
+   **/
+  _.each(innerResult, (innerResultItemQuery, key) => {
+    let root = result[ Op.and ] = result[ Op.and ] || []
+    let transformItemQuery = {
+      [ key ]: { [ Op.and ]: {} }
+    }
+    
+    _.each(innerResultItemQuery, (val, opStr) => {
+      transformItemQuery[ key ][ Op.and ][ Op[ opStr ] ] = val
     })
-  }
+    
+    root.push(transformItemQuery)
+  })
+  
+  return result
 }
